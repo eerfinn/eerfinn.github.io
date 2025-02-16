9c430b93727fc58a34003b3d4a22522b72f6ada8
Erfin Jauhari Dwi Brian <erfinbrian@gmail.com>
2025-02-16 12:44:13 +0700

    home view

---
+++ script.js
@@ -0,0 +1,9 @@
+// Contoh: Smooth scrolling untuk navigasi
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
